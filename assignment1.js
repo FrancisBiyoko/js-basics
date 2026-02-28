2075bb0952cc00249c5dfe23efc68d877e79c674
FRANCIS <fbiyoko999@gmail.com>
2026-02-28 02:38:31 +0000

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+//DECLARING VARIABLES
+let requiredAge=18;
+let userAge="18";
+
+//DECLARING  COMPARISON ==
+console.log(requiredAge == userAge);
+//DECLARING  COMPARISON ===
+console.log(requiredAge === userAge);
+//DECLARING  COMPARISON !==
+console.log(requiredAge == userAge);
+
+
+ //DECLARING  BONUS QUESTION 1
+ console.log(5 >2);
+ //DECLARING  BONUS QUESTION 2
+ console.log(10 <=10);
